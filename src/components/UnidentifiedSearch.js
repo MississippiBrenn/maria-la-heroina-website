abb5589087a4172c0d548eb8587845a584b3f853
Cara <cararbrennan@gmail.com>
2021-01-09 11:34:47 -0800

    github pages

--- src/components/UnidentifiedSearch.js
+++ src/components/UnidentifiedSearch.js
@@ -5,15 +5,15 @@ import { Select, InputLabel, MenuItem, FormControl } from '@material-ui/core';
 
 function UnidentifiedSearch() {
     const [race, setRace] = useState("");
-
     function handleRaceChange(e) {
         e.preventDefault();
         setRace(e.target.value)
 
     }
 
-    const [searchButtonClicked, setSearchButtonClicked] = useState(false)
 
+
+    const [searchButtonClicked, setSearchButtonClicked] = useState(false)
     function handleClick(e) {
         e.preventDefault();
         setSearchButtonClicked(!searchButtonClicked)
@@ -27,13 +27,13 @@ function UnidentifiedSearch() {
                     <MenuItem value={true}>Yes</MenuItem>
                     <MenuItem value={false}>No</MenuItem>
                 </Select>
-                {/* <InputLabel id="radius">Distance between cases</InputLabel>
+                <InputLabel id="radius">Distance between cases</InputLabel>
                 <Select labelId="radius" id="select" value="20">
                     <MenuItem value="10">Less than 10 Miles</MenuItem>
                     <MenuItem value="50">Less than 50 Miles</MenuItem>
                     <MenuItem value="100">Less than 100 Miles</MenuItem>
                     <MenuItem value="-100">Leave this out of my search</MenuItem>
-                </Select> */}
+                </Select>
                 
             </FormControl>
             <p>{`${race}`}</p>
