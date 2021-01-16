b45e1544abd0a6341413548871a7a01f97fcc24e
Cara <cararbrennan@gmail.com>
2021-01-16 14:31:12 -0800

    clean up app file

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,18 @@
 import React, { Component, Fragment } from 'react'
 import { Header, Banner, Footer } from './layouts'
-import SearchMenu from './SearchMenu.js'
-import Exercises from './exercises'
 import Matches from './matches'
 
-import {  matchTo, matchDatabase, muscles, exercises } from "../store.js";
+import {  matchTo, matchDatabase} from "../store.js";
 
 
 export default class extends Component {
     state = {
-      exercises,
-      exercise:{}, 
       matchDatabase,
       matchDBEntry:{}
     }
 
     //accept data as props and pass them down for rendering
     // to be able to return non object for itreating use object entries  
-  getExercisesByMuscles() {
-    return Object.entries(this.state.exercises.reduce((exercises, exercise) => {
-      const { muscles } = exercise
-
-        // spread out elements into a new array 
-      exercises[muscles] = exercises[muscles] 
-      ? [...exercises[muscles], exercise] 
-      : [exercise]
-
-      return exercises
-    }, {})
-    )
-  }
-
   getMatchByCategory() {
     return Object.entries(this.state.matchDatabase.reduce((matchDatabase, matchEntry) => {
       const { type } = matchEntry
@@ -49,15 +31,6 @@ export default class extends Component {
     this.setState({
       category
     })
-  }
-
-  //callback that retrieves previous state(exercises) as well as props
-  //asynchronous setState
-  // to avoid state being changed during call 
-  handleExerciseSelected = id  => {
-    this.setState(({ exercises }) => ({
-      exercise: exercises.find(ex => ex.id === id )
-    }))
   }
 
    //callback that retrieves previous state(exercises) as well as props
@@ -82,13 +55,6 @@ export default class extends Component {
     <Fragment>
       <Header />
       {/* <Banner /> */}
-      {/* <Exercises
-      exercise = {exercise}
-      category={category} 
-      exercises={exercises}
-      onSelect={this.handleExerciseSelected}
-      /> */}
-      {/* <SearchMenu /> */}
       <Matches 
       matchDBEntry={matchDBEntry}
       category={category}
@@ -98,7 +64,6 @@ export default class extends Component {
       <Footer 
       matchTo={matchTo}
       category={category}
-      muscles={muscles}
       onSelect={this.handleCategorySelected}/>
     </Fragment>
     )  
